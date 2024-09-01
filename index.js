b2dffd0422812b77f616070a7d4621fc2830c6d7
Giovanne Ferrari <vanneferrarii@hotmail.com>
2024-09-01 14:32:36 +0000

    melhorias no

--- index.js
+++ index.js
@@ -215,121 +215,6 @@ async function converterMoeda() {
       "info"
     ).textContent = `Cotação atual: $ ${taxaDeCambioFormatada} (Atualizado em: ${dataCotacao}) \nFonte: Banco Central do Brasil (https://olinda.bcb.gov.br).`;
 
-    async function converterMoeda() {
-      const valor = document.getElementById("valor").value;
-      const conversionType = document.getElementById("conversionType").value;
-
-      if (valor === "") {
-        alert("Por favor, insira um valor.");
-        return;
-      }
-
-      try {
-        let today = new Date();
-
-        // Retroceder até encontrar um dia útil
-        while (!isWeekday(today)) {
-          today.setDate(today.getDate() - 1);
-        }
-
-        let formattedDate = `${String(today.getMonth() + 1).padStart(
-          2,
-          "0"
-        )}-${String(today.getDate() + 1).padStart(2, "0")}-${String(
-          today.getFullYear()
-        )}`;
-
-        let apiUrl;
-        let moeda;
-        if (conversionType === "BRLtoUSD" || conversionType === "USDtoBRL") {
-          moeda = "USD";
-          apiUrl = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27${formattedDate}%27&$top=100&$skip=0&$format=json&$select=cotacaoVenda`;
-        } else if (
-          conversionType === "BRLtoEUR" ||
-          conversionType === "EURtoBRL"
-        ) {
-          moeda = "EUR";
-          apiUrl = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda=%27EUR%27&@dataCotacao=%27${formattedDate}%27&$top=5&$skip=4&$format=json&$select=cotacaoVenda`;
-        }
-
-        let response = await fetch(apiUrl);
-        let jsonData = await response.json();
-
-        // Continuar retrocedendo até encontrar uma cotação válida
-        while (jsonData.value.length === 0) {
-          today.setDate(today.getDate() - 1);
-
-          // Certifique-se de que ainda está no intervalo de dias úteis.
-          while (!isWeekday(today)) {
-            today.setDate(today.getDate() - 1);
-          }
-
-          formattedDate = `${String(today.getMonth() + 1).padStart(
-            2,
-            "0"
-          )}-${String(today.getDate()).padStart(2, "0")}-${String(
-            today.getFullYear()
-          )}`;
-
-          if (moeda === "USD") {
-            apiUrl = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoDolarDia(dataCotacao=@dataCotacao)?@dataCotacao=%27${formattedDate}%27&$top=100&$skip=0&$format=json&$select=cotacaoVenda`;
-          } else if (moeda === "EUR") {
-            apiUrl = `https://olinda.bcb.gov.br/olinda/servico/PTAX/versao/v1/odata/CotacaoMoedaDia(moeda=@moeda,dataCotacao=@dataCotacao)?@moeda=%27EUR%27&@dataCotacao=%27${formattedDate}%27&$top=5&$skip=4&$format=json&$select=cotacaoVenda`;
-          }
-
-          response = await fetch(apiUrl);
-          jsonData = await response.json();
-        }
-
-        const taxaDeCambio = parseFloat(jsonData.value[0].cotacaoVenda);
-        const dataCotacao = formatDate(today);
-
-        let resultado;
-        if (conversionType === "BRLtoUSD") {
-          const valorEmDolar = (valor / taxaDeCambio).toLocaleString("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 4,
-          });
-          resultado = `R$ ${valor} equivale a $ ${valorEmDolar}`;
-        } else if (conversionType === "USDtoBRL") {
-          const valorEmReais = (valor * taxaDeCambio).toLocaleString("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 4,
-          });
-          resultado = `$ ${valor} equivale a R$ ${valorEmReais}`;
-        } else if (conversionType === "BRLtoEUR") {
-          const valorEmEuro = (valor / taxaDeCambio).toLocaleString("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 4,
-          });
-          resultado = `R$ ${valor} equivale a € ${valorEmEuro}`;
-        } else if (conversionType === "EURtoBRL") {
-          const valorEmReais = (valor * taxaDeCambio).toLocaleString("pt-BR", {
-            minimumFractionDigits: 2,
-            maximumFractionDigits: 4,
-          });
-          resultado = `€ ${valor} equivale a R$ ${valorEmReais}`;
-        }
-
-        const taxaDeCambioFormatada = taxaDeCambio.toLocaleString("pt-BR", {
-          minimumFractionDigits: 2,
-          maximumFractionDigits: 4,
-        });
-        document.getElementById("resultado").textContent = resultado;
-        document.getElementById(
-          "info"
-        ).textContent = `Cotação atual: $ ${taxaDeCambioFormatada} (Atualizado em: ${dataCotacao}) \nFonte: Banco Central do Brasil (https://olinda.bcb.gov.br).`;
-
-        // Exibir o histórico da moeda selecionada
-        exibirHistorico(moeda);
-      } catch (error) {
-        document.getElementById("resultado").textContent =
-          "Erro ao obter a taxa de câmbio. Tente novamente mais tarde.";
-        document.getElementById("info").textContent = "";
-        document.getElementById("historico").innerHTML = ""; // Limpar o histórico em caso de erro
-      }
-    }
-
     // Exibir o histórico da moeda selecionada
     exibirHistorico(moeda);
   } catch (error) {
@@ -346,6 +231,15 @@ const textarea = document.getElementById("valor");
 textarea.addEventListener("keydown", function (event) {
   if (event.key === "Enter") {
     event.preventDefault();
-    converterMoeda(); // Call the outer converterMoeda function
+    converterMoeda();
+    const bloco = document.querySelector("#historico-box");
+    bloco.style.display = "block";
   }
 });
+
+document
+  .querySelector("#botão")
+  .addEventListener("click", function converterMoeda() {
+    const bloco = document.querySelector("#historico-box");
+    bloco.style.display = "block";
+  });
